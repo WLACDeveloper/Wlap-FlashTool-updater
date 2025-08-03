e6ad188e318493795db8c288a275002d3119116b
WLACDeveloper <WLAPCreatorLite@gmail.com>
2025-08-03 22:11:06 +0300

    Update update.py

--- update.py
+++ update.py
@@ -2,6 +2,6 @@ version = 1.00
 branch = 'main'
 #Если будет версии патча отличаться, то можно скачать обнову
 patch = 2
-repository = 'https://raw.githubusercontent.com/WLACDeveloper/Wlap-FlashTool/refs/heads/Full/'
+repository = 'https://raw.githubusercontent.com/WLACDeveloper/Wlap-FlashTool/refs/heads/'
 #Если версия ниже минимальной, то начнется скачивание полной версии программыы
 update_packages=['image/arch.png']
